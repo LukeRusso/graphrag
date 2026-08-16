7cb4f0a6a1c6ec37ea0696050edeb061731caa44
Luke Russo <crispy.caterpillar@gmail.com>
2026-08-16 10:16:17 +1000

    Update IClusteringAlgorithm.cluster type signature

--- packages/graphrag/graphrag/graphs/clustering/base_clustering_algorithm.py
+++ packages/graphrag/graphrag/graphs/clustering/base_clustering_algorithm.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
-import pandas as pd
+from graphrag.config.models.cluster_graph_config import ClusterGraphConfig
+from graphrag.graphs.type_aliases import Edge
 
 from .cluster import Cluster
 
@@ -9,5 +10,5 @@ class IClusteringAlgorithm(ABC):
     """Interface for hierarchical clustering algorithm."""
 
     @abstractmethod
-    def cluster(self, edges: pd.DataFrame) -> list[Cluster]:
+    def cluster(self, edges: list[Edge], config: ClusterGraphConfig) -> list[Cluster]:
         """Perform hierarchical clustering on input graph."""
